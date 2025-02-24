8feb217623e0e02209e45bf88d6061120b629951
Guilherme Kaludin <guikaludin@outlook.com>
2025-02-24 10:47:30 -0300

    Criando os componentes

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,19 @@
+function Navbar() {
+    return (
+        <>
+            <div className='w-full flex justify-center py-4
+            			   bg-cyan-600 text-white'>
+            
+                <div className="container flex justify-between text-lg">
+                    Farmacia
+
+                    <div className='flex gap-4'>
+                        Categoria
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Navbar
