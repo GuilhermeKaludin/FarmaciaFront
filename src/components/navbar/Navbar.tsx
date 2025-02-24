9709eafb6598431efc1823f85630eaeb30437d91
Guilherme Kaludin <guikaludin@outlook.com>
2025-02-24 11:11:24 -0300

    Ajustando componentes

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom"
+
 function Navbar() {
     return (
         <>
@@ -5,7 +7,7 @@ function Navbar() {
             			   bg-cyan-600 text-white'>
             
                 <div className="container flex justify-between text-lg">
-                    Farmacia
+                <Link to='/home' className="text-2xl font-bold">Farmacia</Link>
 
                     <div className='flex gap-4'>
                         Categoria
